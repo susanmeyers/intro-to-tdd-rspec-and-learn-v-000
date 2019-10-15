ede1e4fb613579607226d67efa16a5875c74ae51
Susan Meyers <smmeyers24@gmail.com>
2019-10-15 04:23:57 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,3 +1,8 @@
 def current_age_for_birth_year(birth_year)
+<<<<<<< HEAD
   Time.now.year - birth_year
+=======
+  2003 - birth_year
+  
+>>>>>>> 869b19f2d0b08658fd6f0083b75c1d0f16684ad5
 end
--- how_old_are_you.rb
+++ how_old_are_you.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require_relative './current_age_for_birth_year.rb'
  
 puts "What year were you born?"
@@ -6,4 +7,17 @@ birth_year = gets.to_i
 users_age = current_age_for_birth_year(birth_year)
  
 puts "You are: " + users_age.to_s + " years old."
+=======
+require_relative '../current_age_for_birth_year.rb'
+ 
+describe "current_age_for_birth_year method" do
+  it "returns the age of a person based on the year of birth" do
+    current_year = Time.now.year
+    birth_year = 1984
+    answer = current_year - birth_year
+ 
+    age_of_person = current_age_for_birth_year(birth_year)
+    expect(age_of_person).to eq(answer)
+  end
+>>>>>>> 869b19f2d0b08658fd6f0083b75c1d0f16684ad5
 end
